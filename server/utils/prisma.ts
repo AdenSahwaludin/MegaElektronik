32c0631ffb1d461860cf3154e141e3fae36e89e7
adensahwaludin <sekha1211@gmail.com>
2026-05-03 15:11:30 +0700

    fix: correct adapter export name PrismaLibSQL for v6 (was PrismaLibSql for v7)

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { createClient } from "@libsql/client/web";
 
 let prismaInstance: PrismaClient | null = null;
@@ -21,7 +21,7 @@ export function getPrismaClient(): PrismaClient {
         authToken: authToken,
       });
 
-      const adapter = new PrismaLibSql(libSql);
+      const adapter = new PrismaLibSQL(libSql);
 
       prismaInstance = new PrismaClient({
         adapter,
